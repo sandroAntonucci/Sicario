9970fba8f127cb36beb588b1be0e017a495af9b5
Devream <devream24@gmail.com>
2025-05-16 20:42:49 +0200

    Update

--- Assets/NextLevelDetectoer.cs
+++ Assets/NextLevelDetectoer.cs
@@ -5,11 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class NextLevelDetector : MonoBehaviour
 {
+    public GameObject backgroundMusic;
+
+    private void Start()
+    {
+        if (backgroundMusic == null)
+        {
+            backgroundMusic = GameObject.FindGameObjectWithTag("BackgroundMusic");
+        }
+
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            Destroy(backgroundMusic);
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
